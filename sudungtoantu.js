a9f06ca57594e94e57d84c6301e0b5ebdd4f52b3
unknown <you@example.com>
2022-11-08 20:16:08 +0700

    Add sudungtoantu.html file

---
+++ sudungtoantu.js
@@ -0,0 +1,8 @@
+let inputWidth;
+let inputHeight;
+inputHeight=prompt("Enter the Height");
+inputWidth=prompt("Enter the Width");
+let Width=parseInt(inputWidth);
+let Height=parseInt(inputHeight);
+let Area = Width*Height
+document.write("The area is: "+ Area)
